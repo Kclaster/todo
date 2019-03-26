572553db8acd5b6ebab3ab48cceffa993a3d7d87
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-03-25 21:15:49 -0500

    footer

--- client/src/components/features/Footer/index.js
+++ client/src/components/features/Footer/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import coolpic from '../../../pictures/githubicon.png';
 import './style.css';
 
 class Footer extends React.Component {
     render(){
         return(
             <div class="footer">
-            <img className='githubIcon' src='../../pictures/githubicon.png' alt='icon' />
-            < h1> Copyright 2019 The Dream Team </h1>
+            <img class='githubIcon' src={coolpic} alt='icon' />
+            < h3> Copyright 2019 The Dream Team </h3>
             </div>
+            
         )
     }
 }
