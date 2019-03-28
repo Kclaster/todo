bcbc08433adf7a6150c289a523f216d7a70b7530
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-03-27 20:06:24 -0500

    AMERICAgit commit git commit git commit git commit git commit git commit !

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'Elijah@2018',
+  password: 'Diddy666',
   database: 'users'
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-import Mysql from 'mysql';
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
