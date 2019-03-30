d94c4498d09582811710153dc053a4c141529485
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-03-30 12:01:29 -0500

    photo

--- client/src/components/features/Footer/index.js
+++ client/src/components/features/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import coolpic from '../../../pictures/githubicon.png';
+import coolpic from '../../../pictures/githubIcon';
 import './style.css';
 
 class Footer extends React.Component {
