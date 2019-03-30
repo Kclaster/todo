f24b35f751e69c5c862b3aa0555deb459703f314
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-03-30 11:05:34 -0500

    Pushing Moment js Date Checker

--- client/src/components/features/Sidebar/MarketPlaceContainer/MarketPlace/MarketPlaceSnippet/index.js
+++ client/src/components/features/Sidebar/MarketPlaceContainer/MarketPlace/MarketPlaceSnippet/index.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import './style.css';
+import moment from 'moment';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 
-const MarketPlaceSnippet = props => {
+class MarketPlaceSnippet extends React.Component {
+ constructor(props){
+   super(props);
+   this.state ={
+     todaysDate: new Date(),
+     expireDate: this.props.expiration,
+     isAfter: null,
+     newBid: 0
+   };
+ }
+
+  static getDerivedStateFromProps(nextProps, prevProps) {
+    if (prevProps.expiration !== nextProps.expiration) {
+      return {
+        isAfter: moment(new Date()).isAfter(nextProps.expiration)
+      };
+    }
+  }
+
+  render() {
   return (
     <div className="market-snippet-container">
  <ExpansionPanelSummary >
      <ExpansionPanelDetails>
           <Typography>
-      <h3 className="market-title-link" onClick={props.toggleSnippet}>
-        {props.title}
+      <h3 className="market-title-link" onClick={this.props.toggleSnippet}>
+        {this.props.title}
       </h3>
-      <h4>Expiration: {props.expiration}</h4>
-      <h4>Current Bid: {props.startingBid}</h4>
-      <p>Description: {props.description}</p>
+      <h4>Expiration: {this.props.expiration}</h4>
+      <h4>Current Bid: {this.props.startingBid}</h4>
+      <p>Description: {this.props.description}</p>
       <form>
         <input
-          onChange={e => props.handleChange(e)}
+          onChange={e => this.props.handleChange(e)}
           type="text"
           placeholder="Your Bid"
           />
@@ -29,6 +49,7 @@ const MarketPlaceSnippet = props => {
         </ExpansionPanelSummary>
     </div>
   );
+  };
 };
 
 export default MarketPlaceSnippet;
