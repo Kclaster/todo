3b1a8568146615f7976684a58add02bd6cb1df50
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-04-01 00:12:35 -0500

    Updated website info

--- client/src/components/mainPages/HomePage/ReviewContainer/index.js
+++ client/src/components/mainPages/HomePage/ReviewContainer/index.js
@@ -29,12 +29,15 @@ class ReviewContainer extends React.Component {
 
     this.aboutContent1 = {
       header: 'Why TaskBidder',
-      text: "We believe laziness drives the American economy, and why shouldn't it? You have tasks that needs to get done and there are people willing to do them. ",
+      text: "How much is your time really worth? For many Americans, time is in short supply. Whether it's balancing between your busy work schedual, or full social life or simply watching after a family, it can get tough to check off every chore on your to do list. TaskBidder allows for you to put those pesky time consuming chores up for sale, and allows other to become the extra hands you need! You have tasks that needs to get done and there are people willing to do them",
     }
+
+    //"We believe laziness drives the American economy, and why shouldn't it? You have tasks that needs to get done and there are people willing to do them. "
+
 
     this.aboutContent2 = {
       header: 'How to use TaskBidder',
-      text: 'TaskBidder is simple to use! Simply create a task with a fair starting bid and let others bid lower to try and win that task. ',
+      text: 'TaskBidder is simple to use! Simply create a task with a fair starting bid and send it off to our marketplace. Other users will bid for your task! At the end, you both leave reviews about each other and the quality of work done, which helps users build positive ratings, and allows everyone to maintain a great evironment',
     }
 
 
@@ -53,19 +56,7 @@ class ReviewContainer extends React.Component {
 
 
       })
-      // console.log('akunea')
-      // axios.get('/user/reviews/yourmom')
-      //   .then(res => {
-      // console.log(res)
-      //     this.setState({
-      //       review: res.data
-    
-    
-      //     })
-    
-      //   })
   }
-
 
 
 
@@ -76,10 +67,10 @@ class ReviewContainer extends React.Component {
     if (this.state.displayChild) {
       display =
         <div className="review-container">
-          <Review data={this.state.review} users={(this.state.arr) ? this.state.arr.slice(0, 3) : null} />
+          <Review users={(this.state.arr) ? this.state.arr.slice(0, 3) : null} />
           <About content={this.aboutContent1} />
           <About content={this.aboutContent2} />
-          <Review  data={this.state.review} users={(this.state.arr) ? this.state.arr.slice(3, 6) : null} />
+          <Review users={(this.state.arr) ? this.state.arr.slice(3, 6) : null} />
         </div>
     } else {
       display = null
