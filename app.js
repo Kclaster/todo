4a7907ca051147fad70bfaf6c096d5fe13b6f120
Kyle Laster <kyle.laster@gmail.com>
2019-04-01 21:09:03 -0500

    fix deployment

--- app.js
+++ app.js
@@ -18,8 +18,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+// app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 app.use('/', indexRouter);
 app.use('/todos', todosRouter);
 app.use('/user', userRouter);
