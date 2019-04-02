74a1e875bfdf16fdd845dcd4645687d7d06925d6
Kyle Laster <kyle.laster@gmail.com>
2019-04-01 20:21:47 -0500

    bug fix

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'Elijah@2018',
-  database: 'users'
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'users'
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
